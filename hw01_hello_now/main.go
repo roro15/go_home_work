6f76f81653e38771f60c7fe895fcbd1d6ad6aa29
Roman Peshekhonov <roro15@yandex.ru>
2020-03-09 17:24:21 +0300

    Done homework 1

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const ntpHost = "0.beevik-ntp.pool.ntp.org"
+const timeLayout = "2006-01-02 15:04:05 -0700 MST"
+
 func main() {
-	// Place your code here
+	exactTime, err := ntp.Time(ntpHost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentTime := time.Now()
+
+	fmt.Printf("current time: %s\n", currentTime.Format(timeLayout))
+	fmt.Printf("exact time: %s\n", exactTime.Format(timeLayout))
 }
